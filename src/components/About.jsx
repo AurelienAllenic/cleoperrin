459d29bad64d9a4604b6a0c8dfa2eb873ceeb454
Aurelien Allenic <aurelien.allenic@gmail.com>
2022-12-12 18:53:32 +0100

    (feat) added elements for the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Parallax } from 'react-parallax';
 import AboutIMG from "../assets/about.jpg"
+import Chair from "../assets/chair.jpg"
+import { P, Img, Div } from "../style/About"
 
 function About() {
     return (
         <>
             <section id="about">
                 <Parallax className='img' bgImage={AboutIMG} strength={800}>
+                    <Div>
+                        <P>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum necessitatibus alias praesentium eligendi quod doloribus, ab possimus quae nemo voluptatibus at, mollitia perspiciatis reiciendis unde eum illo inventore cum beatae.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum necessitatibus alias praesentium eligendi quod doloribus, ab possimus quae nemo voluptatibus at, mollitia perspiciatis reiciendis unde eum illo inventore cum beatae.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum necessitatibus alias praesentium eligendi quod doloribus, ab possimus quae nemo voluptatibus at, mollitia perspiciatis reiciendis unde eum illo inventore cum beatae.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum necessitatibus alias praesentium eligendi quod doloribus, ab possimus quae nemo voluptatibus at, mollitia perspiciatis reiciendis unde eum illo inventore cum beatae.</P>
+                        <Img src={Chair} alt="chair" />
+                    </Div>
                 </Parallax>
             </section>
         </>
---
+++ src/style/About.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+display: grid;
+grid-template-columns: repeat(2, 1fr);
+width: 100%;
+position: relative;
+top: 180px;
+left: 100px;
+`
+export const P = styled.p`
+font-size: 22px;
+width: 50%;
+font-weight: 500;
+line-height: 30px;
+`
+export const Img = styled.img`
+    height: 500px;
+    position: relative;
+    top: 100px;
+    border-radius: 10px 10px 10px 10px;
+`
