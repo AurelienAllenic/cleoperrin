6f968d3831dbd51d1a0ef859f801bedf7e71d70b
Aurelien Allenic <aurelien.allenic@gmail.com>
2022-12-14 16:44:50 +0100

    (feat) added new responsive navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,22 +1,40 @@
 import React from 'react'
+import "./nav.css"
+import { useState } from "react"
 import { Link } from "react-scroll"
-import { EntireNav, Ul, Li } from "../../style/Nav"
 
-function Nav() {
+function Navbar() {
+
+    const [showLinks, setShowlinks] = useState(false)
+
+    const handleShowLinks = () => {
+        setShowlinks(!showLinks)
+    }
     return (
-        <>
-            <EntireNav>
-                <Ul>
-                    <Li><Link to='about' spy={true} smooth={true} offset={0} duration={500}>Cléo Perrin</Link></Li>
-                    <Li><Link to='about' spy={true} smooth={true} offset={0} duration={500}>A Propos</Link></Li>
-                    <Li><Link to='design' spy={true} smooth={true} offset={0} duration={500}>Design</Link></Li>
-                    <Li><Link to='space' spy={true} smooth={true} offset={0} duration={500}>Espaces Publics</Link></Li>
-                    <Li><Link to='residential' spy={true} smooth={true} offset={0} duration={500}>Résidentiel</Link></Li>
-                    <Li><Link to='contact' spy={true} smooth={true} offset={0} duration={500}>Contact</Link></Li>
-                </Ul>
-            </EntireNav>
-        </>
+        <div className={`navbar ${showLinks ? "show-nav" : "hide-nav"}`}>
+            <Link className='navbar_logo' to='about' spy={true} smooth={true} offset={0} duration={500} onClick={handleShowLinks}>Cléo Perrin</Link>
+            <ul className='navbar_links'>
+                <li className='navbar_item slideInDown-1'>
+                    <Link className='navbar_link' to='about' spy={true} smooth={true} offset={0} duration={500} onClick={handleShowLinks}>A Propos</Link>
+                </li>
+                <li className='navbar_item slideInDown-2'>
+                    <Link className='navbar_link' to='design' spy={true} smooth={true} offset={0} duration={500} onClick={handleShowLinks}>Design</Link>
+                </li>
+                <li className='navbar_item slideInDown-3'>
+                    <Link className='navbar_link' to='space' spy={true} smooth={true} offset={0} duration={500} onClick={handleShowLinks}>Espaces Publics</Link>
+                </li>
+                <li className='navbar_item slideInDown-4'>
+                    <Link className='navbar_link' to='residential' spy={true} smooth={true} offset={0} duration={500} onClick={handleShowLinks}>Résidentiel</Link>
+                </li>
+                <li className='navbar_item slideInDown-5'>
+                    <Link className='navbar_link' to='contact' spy={true} smooth={true} offset={0} duration={500} onClick={handleShowLinks}>Contact</Link>
+                </li>
+            </ul>
+            <button className='navbar_burger' onClick={handleShowLinks}>
+                <span className='burger-bar'></span>
+            </button>
+        </div>
     )
 }
 
-export default Nav
+export default Navbar
