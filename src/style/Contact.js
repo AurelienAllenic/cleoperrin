78ac79bc498bd12cf44598059e1f6766cea9ae85
Aurelien Allenic <aurelien.allenic@gmail.com>
2022-12-14 17:59:26 +0100

    (feat) added media queries for contact section

--- src/style/Contact.js
+++ src/style/Contact.js
@@ -9,6 +9,10 @@ bottom: 60px;
 flex-direction: column;
 width: 100%;
 height: 82vh;
+@media (max-width: 1492px){
+    height: 100vh;
+    bottom: 0px;
+}
 `
 export const InputDiv = styled.div`
 `
@@ -21,6 +25,21 @@ border-left: none;
 border-right: none;
 background-color: transparent;
 font-size: 25px;
+@media (max-width: 647px){
+    width: 150px;
+    font-size: 17px;
+    margin: 30px;
+}
+@media (max-width: 427px){
+    width: 120px;
+}
+@media (max-width: 367px){
+    width: 100px;
+    font-size: 15px;
+}
+@media (max-width: 327px){
+    width: 250px;
+}
 &::placeholder{
     color: black;
 }
@@ -45,6 +64,12 @@ resize: none;
 font-size: 25px;
 background-color: transparent;
 border: 2px solid black;
+@media (max-width: 1124px){
+    width: 50%;
+}
+@media (max-width: 647px){
+    width: 80%;
+}
 &::placeholder{
     color: black;
     text-align: center;
@@ -55,6 +80,10 @@ border: 2px solid black;
     &::placeholder{
         color: white;
     }
+    &:focus{
+        outline: none;
+    }
+
 }
 `
 export const Button = styled.button`
@@ -67,6 +96,9 @@ border-right: none;
 border-bottom: 1px solid black;
 background-color : transparent;
 font-size: 20px;
+@media (max-width: 793px){
+    border-bottom: none;
+}
 &:hover{
     border-bottom: 2px solid white;
     cursor: pointer;
