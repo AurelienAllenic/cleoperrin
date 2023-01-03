a72cc4073bd6f02cb8f39b7c792b6b6419fb2d2c
Aurelien Allenic <aurelien.allenic@gmail.com>
2023-01-03 17:05:19 +0100

    (feat) added loader

--- src/App.js
+++ src/App.js
@@ -6,10 +6,32 @@ import { home } from "./components/Data";
 import Home from "./components/Home";
 import FooterContact from './components/FooterContact';
 import ProgressBar from "./components/ProgressBar";
+import SyncLoader from "react-spinners/SyncLoader";
+import {useState, useEffect} from "react"
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  useEffect(()=>{
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    }, 3500)
+  }, [])
   return (
     <div className="App">
+       {
+        loading ?
+        <div className="loader">
+        <SyncLoader
+        loading={loading}
+        size={30}
+        color="black"
+        aria-label="Loading Spinner"
+        data-testid="loader"
+        speedMultiplier={0.5}
+      /></div>
+      :
+      <>
       <Nav/>
       <ProgressBar/>
       <Home images={home}/>
@@ -17,6 +39,8 @@ function App() {
       <Design/>
       <Contact/>
       <FooterContact/>
+      </>
+}
     </div>
   );
 }
